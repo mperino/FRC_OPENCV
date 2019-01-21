e298d8b160e6af6d55fbdcb476541da4a2986fd8
mperino <mark.perino@gmail.com>
2019-01-21 12:24:50 -0800

    Add files via upload
    update to show both circles and vision targets

--- video test.py
+++ video test.py
@@ -1,5 +1,7 @@
 import cv2
 import time
+import numpy as np
+
 
 # Load in a video file, or access the camera use 'filename.mp4" for file, or a number for a camera
 cap = cv2.VideoCapture('WIN_20190119_14_27_53_Pro.mp4')
@@ -16,10 +18,16 @@ while True:
     ret, frame=cap.read()
 # convert the frame to greyscale as well
     grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-# draw a 1 pixel thick red (but since it's grey it'll be black) circle of radius 220 in the center of the video
-    cv2.circle(grey,(int(cols/2),int(rows/2)), 220, (0,0,255), 1)
-# draw a bullseye circle of 1 pixel thick, radius 4 in the center of the video feed
+    # draw a 1 pixel thick red (but since it's grey it'll be black) circle of radius 220 in the center of the video
+    cv2.circle(grey, (int(cols / 2), int(rows / 2)), 220, (0, 0, 255), 1)
+    # draw a bullseye circle of 1 pixel thick, radius 4 in the center of the video feed
     cv2.circle(grey, (int(cols / 2), int(rows / 2)), 4, (0, 0, 255), 1)
+    # draw a vision target left box
+    lbpts = np.array([[451, 158], [488, 18], [541, 32], [502, 175]], np.int32)
+    cv2.polylines(grey, [lbpts], True, (0, 0, 255), 1)
+    # draw a vision target for the right box
+    rbpts = np.array([[779, 175], [750, 32], [805, 18], [835, 160]], np.int32)
+    cv2.polylines(grey, [rbpts], True, (0, 0, 255), 1)
 # display both the color and grayscale
     cv2.imshow('frame', frame)
     cv2.imshow('gray', grey)
