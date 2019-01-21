63405aab81d4b915dc23be75184fb82f34154736
mperino <mark.perino@gmail.com>
2019-01-21 11:23:09 -0800

    Add files via upload

---
+++ video test.py
@@ -0,0 +1,34 @@
+import cv2
+import time
+
+# Load in a video file, or access the camera use 'filename.mp4" for file, or a number for a camera
+cap = cv2.VideoCapture('WIN_20190119_14_27_53_Pro.mp4')
+
+#extract the first frame
+ret, frame=cap.read()
+
+# show the dimensions of the video
+rows, cols, channels = frame.shape
+print ("Video is:{0} Pixels Wide, {1} Pixels High, and {2} Channels".format(rows, cols, channels))
+
+# Keep extracting frames in a loop until end of video or "q" is pressed.
+while True:
+    ret, frame=cap.read()
+# convert the frame to greyscale as well
+    grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+# draw a 1 pixel thick red (but since it's grey it'll be black) circle of radius 220 in the center of the video
+    cv2.circle(grey,(int(cols/2),int(rows/2)), 220, (0,0,255), 1)
+# draw a bullseye circle of 1 pixel thick, radius 4 in the center of the video feed
+    cv2.circle(grey, (int(cols / 2), int(rows / 2)), 4, (0, 0, 255), 1)
+# display both the color and grayscale
+    cv2.imshow('frame', frame)
+    cv2.imshow('gray', grey)
+# quit the loop if 'q' is pressed
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+# wait 30 sec, and then die
+        time.sleep(30)
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
